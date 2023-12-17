e41cd3c5e48cc7879aaf2219afbab3eb72a9d6b3
Vishal Sharma <vishalamt1@gmail.com>
2023-12-17 07:44:07 +0000

    testing commiting

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("hello mukul")
+let man = "this is a string that contains"
+.split("")
+.reverse()
+.map(parseInt)
+.filter(a => a)
+.reduce((a,b) => a+b)
+.toString()
+.split("")
+.reverse()
+.join("")
+
+console.log(man)
