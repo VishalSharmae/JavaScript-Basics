df5760e9f91fe470698fff8ffca8791a47b3de09
Vishal Sharma <work.vishal1798@gmail.com>
2023-12-17 21:47:40 +0000

    added variables and datatypes

--- learningBasicsJS/src/main/variables.js
+++ learningBasicsJS/src/main/variables.js
@@ -8,4 +8,15 @@ const c = "c"
 
 console.log(a, b, c)
 
-
+let number = 5
+let string = "string"
+let boolean = true
+let undef = undefined
+let nulll = null
+let nullDataType = null
+let bigInt = BigInt(564)
+let symbol = Symbol("symbol")
+let object = {
+  name: "object",
+  age: 5
+}
