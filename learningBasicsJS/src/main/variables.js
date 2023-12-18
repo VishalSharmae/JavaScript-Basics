c10043ce8394a9461281d24c71cfc4dfd4348bbc
Vishal Sharma <98262595+VishalSharmae@users.noreply.github.com>
2023-12-19 00:35:39 +0530

    Adding changes to Prod (#1)
    * testing commiting
    
    * checking commit
    
    * added variables and datatypes
    
    * questions.js added
    
    ---------
    
    Co-authored-by: Vishal Sharma <vishalamt1@gmail.com>

---
+++ learningBasicsJS/src/main/variables.js
@@ -0,0 +1,22 @@
+// nn bb ss u
+
+// var , let, const
+
+var a = "a"
+let b = "b"
+const c = "c"
+
+console.log(a, b, c)
+
+let number = 5
+let string = "string"
+let boolean = true
+let undef = undefined
+let nulll = null
+let nullDataType = null
+let bigInt = BigInt(564)
+let symbol = Symbol("symbol")
+let object = {
+  name: "object",
+  age: 5
+}
